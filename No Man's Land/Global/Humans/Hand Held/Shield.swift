b10d8b7fee4b8cb623f358c48b0b30664079585b
Traxido <andrew.sheron@gmail.com>
2019-12-26 18:14:36 -0500

    Created Shield: Copying, and Initialization

--- No Man's Land/Global/Humans/Hand Held/Shield.swift
+++ No Man's Land/Global/Humans/Hand Held/Shield.swift
@@ -10,8 +10,44 @@ import UIKit
 import SpriteKit
 
 class Shield: SKSpriteNode {
+    var imageName = String()
     var chanceOfNoHit = 1
-//    override func copy(with zone: NSZone? = nil) -> Any {
-//        <#code#>
-//    }
+    var staticImage = SKTexture()
+    var runningAnimation: [SKTexture] = []
+    var blockingAnimation: [SKTexture] = []
+    
+    convenience init(image: String, chance: Int) {
+        self.init()
+        
+        staticImage = SKTexture.init(imageNamed: image)
+        imageName = image
+        chanceOfNoHit = chance
+        
+        for i in 0...3 {
+            runningAnimation.append(SKTexture.init(imageNamed: "\(image)Running\(i)"))
+            blockingAnimation.append(SKTexture.init(imageNamed: "\(image)Block\(i)"))
+        }
+        
+    }
+    
+    func animate(animation: String) {
+        
+        var animate = SKAction()
+        
+        if animation == "run" {
+            animate = SKAction.animate(with: runningAnimation, timePerFrame: 0.15)
+            self.run(.repeatForever(animate))
+        } else if animation == "block" {
+            animate = SKAction.animate(with: blockingAnimation, timePerFrame: 0.15)
+            self.run(.repeatForever(animate))
+        } else if animation == "idle" {
+            self.removeAllActions()
+            self.texture = staticImage
+        }
+    }
+    
+    override func copy(with zone: NSZone? = nil) -> Any {
+        let copy = Shield.init(image: imageName, chance: chanceOfNoHit)
+        return copy
+    }
 }
