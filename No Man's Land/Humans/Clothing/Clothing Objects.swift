5c3b20a9ba95e62527d64f11b541624a30b46e62
Traxido <andrew.sheron@gmail.com>
2019-12-26 21:09:13 -0500

    Moved to "All Objects.swift"

--- No Man's Land/Humans/Clothing/Clothing Objects.swift
+++ No Man's Land/Humans/Clothing/Clothing Objects.swift
@@ -9,33 +9,6 @@
 import Foundation
 import SpriteKit
 
-//Hats
-var helmets : [Helmet] = [ironHelmet1, ironHelmet2, ironHelmet3, ironHelmet4, ironHelmet5, ironHelmet6]
-
-//Shirts
-var allShirts : [Shirt] = [redShirt, darkBrownShirt, ivoryShirt, lightBrownShirt, offBlackShirt, purpleShirt]
-var villagerShirts : [Shirt] = [darkBrownShirt, ivoryShirt, lightBrownShirt]
-var knightShirts : [Shirt] = [darkBrownShirt, ivoryShirt, lightBrownShirt, offBlackShirt]
-
-//Pants
-var allPants : [Pants] = [lightPants, darkPants, brownPants]
-
-//Sleeves
-var caucasianSleeves : [Sleeves] = [caucasianSleeveDarkBrown, caucasianSleeveIvory, caucasianSleeveLightBrown, caucasianSleeveLightRed, caucasianSleeveMetal, caucasianSleevePurple]
-var africanAmericanSleeves : [Sleeves] = [africanAmericanSleeveDarkBrown, africanAmericanSleeveIvory, africanAmericanSleeveLightBrown, africanAmericanSleeveLightRed, africanAmericanSleeveMetal, africanAmericanSleevePurple]
-
-var caucasianVillagerSleeves : [Sleeves] = [caucasianSleeveDarkBrown, caucasianSleeveIvory, caucasianSleeveLightBrown]
-var africanAmericanVillagerSleeves : [Sleeves] = [africanAmericanSleeveDarkBrown, africanAmericanSleeveIvory, africanAmericanSleeveLightBrown]
-
-//Armor
-var armor : [Armor] = [ironArmor1, ironArmor2]
-
-//Accessories
-var longCoats : [Accessory] = [redOverCoat, brownOverCoat, greenOverCoat, purpleOverCoat]
-
-var allTools : [Tool] = []
-var allWeapons : [Weapon] = []
-
 var redShirt = Shirt.init(image: "redShirt")
 var darkBrownShirt = Shirt.init(image: "darkBrownShirt")
 var ivoryShirt = Shirt.init(image: "ivoryShirt")
